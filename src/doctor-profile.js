81c1320e87af24470a8c57cd16d16ffd6352f431
Student <example@example.com>
2019-03-25 18:38:45 -0700

    last classroom push

---
+++ src/doctor-profile.js
@@ -0,0 +1,14 @@
+export class DoctorCard {
+  constructor(name, address, phone, website, accepting){
+    this.name = name;
+    this.address = address;
+    this.phone = phone;
+    this.website = website;
+    this.accepting = accepting;
+  }
+  organizeData(){
+    //continue with doctor-profile or explore template literals
+  }
+
+
+}
